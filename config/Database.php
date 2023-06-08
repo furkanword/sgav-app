67d9e6ec131adfcc6a3dfce54d91e47c09bd756b
furkanword <arcekevin898@gmail.com>
2023-06-08 07:23:14 -0500

    feat: :fire: BD en sql AND postgres

--- config/Database.php
+++ config/Database.php
@@ -1,45 +1,46 @@
 <?php
-    namespace App;
-    class Database{
-        private $conn;
-        protected static = array(
-            "mysql"=> Array(
-                'driver' => 'mysql',
-                'host' => 'localhost',
-                'username' => 'root',
-                'database' => 'sgavBd',
-                'password' => 'campus2023',
-                'collation' => 'utf8mb4_unicode_ci',
-                'flags' => [
-                    // Turn off persistent connections
-                    \PDO::ATTR_PERSISTENT => false,
-                    // Enable exceptions
-                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                    // Emulate prepared statements
-                    \PDO::ATTR_EMULATE_PREPARES => true,
-                    // Set default fetch mode to array
-                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                    // Set character set
-                    \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
-                ],
-                "pgsql"=> Array(
-                    'driver' => 'pgsql',
-                    'host' => 'localhost',
-                    'username' => 'postgres',
-                    'database' => 'mitienda',
-                    'password' => '123456',
-                    'flags' => [
-                        // Turn off persistent connections
-                        \PDO::ATTR_PERSISTENT => false,
-                        // Enable exceptions
-                        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                        // Set default fetch mode to array
-                        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                        // Set character set
-                        \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-                    ]
-                )
-            )
-        );
-    }
+namespace App;
+
+class Database{
+    private $conn;
+    protected static $settings = array(
+        "mysql" => array(
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'username' => 'root',
+            'database' => 'sgavBd',
+            'password' => 'campus2023',
+            'collation' => 'utf8mb4_unicode_ci',
+            'flags' => [
+                // Turn off persistent connections
+                \PDO::ATTR_PERSISTENT => false,
+                // Enable exceptions
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                // Emulate prepared statements
+                \PDO::ATTR_EMULATE_PREPARES => true,
+                // Set default fetch mode to array
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                // Set character set
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+            ]
+        ),
+        "pgsql" => array(
+            'driver' => 'pgsql',
+            'host' => 'localhost',
+            'username' => 'postgres',
+            'database' => 'mitienda',
+            'password' => '123456',
+            'flags' => [
+                // Turn off persistent connections
+                \PDO::ATTR_PERSISTENT => false,
+                // Enable exceptions
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                // Set default fetch mode to array
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                // Set character set
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+            ]
+        )
+    );
+}
 ?>
--- sgavBd.sql
+++ sgavBd.sql
@@ -49,14 +49,72 @@ CREATE TABLE living_place(
     image_living VARCHAR(60) NOT NULL,
     id_typehouse INT NOT NULL,
     CONSTRAINT pk_living_place PRIMARY KEY (id_living),
-    CONSTRAINT fk_cityregions FOREIGN 0 KEY (id_city)REFERENCES city(id_city)
-    
-):
-
+        CONSTRAINT fk_living_place_person PRIMARY KEy (id_person) REFERENCES persons(id_person),
+    CONSTRAINT fk_living_place_city FOREIGN KEY (id_city) REFERENCES cities(id_city),
+    CONSTRAINT fk_living_place_housetype FOREIGN KEY (id_typehouse) REFERENCES housetype(id_hausetype)
+);
 
  
  
+CREATE TABLE housetype(
+    id_hausetype INT NOT NULL AUTO_INCREMENT,
+    name_hausetype VARCHAR(50),
+    CONSTRAINT pk_housetype PRIMARY KEY (id_hausetype),
+    CONSTRAINT uk_housetype_name UNIQUE (name_hausetype)
+);
 
+CREATE TABLE cities (
+    id_city SERIAL PRIMARY KEY,
+    name_city VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE countries (
+    id_country SERIAL PRIMARY KEY,
+    name_country VARCHAR(50) NOT NULL,
+    id_city INT,
+    CONSTRAINT fk_countries_city FOREIGN KEY (id_city) REFERENCES cities(id_city)
+);
+
+CREATE TABLE regions (
+    id_region SERIAL PRIMARY KEY,
+    name_region VARCHAR(50) NOT NULL,
+    id_country INT,
+    CONSTRAINT fk_regions_country FOREIGN KEY (id_country) REFERENCES countries(id_country)
+);
+
+CREATE TABLE persons (
+    id_person VARCHAR(15) PRIMARY KEY,
+    firstname_person VARCHAR(25),
+    lastname_person VARCHAR(25),
+    birthdate_person DATE,
+    id_city INT NOT NULL,
+    CONSTRAINT fk_persons_city FOREIGN KEY (id_city) REFERENCES cities(id_city)
+);
+
+CREATE TABLE housetype (
+    id_hausetype SERIAL PRIMARY KEY,
+    name_hausetype VARCHAR(50),
+    CONSTRAINT uk_housetype_name UNIQUE (name_hausetype)
+);
+
+
+CREATE TABLE living_place (
+    id_living SERIAL PRIMARY KEY,
+    id_person VARCHAR(20) NOT NULL,
+    id_city INT NOT NULL,
+    rooms_living INT NOT NULL,
+    bathrooms_living INT NOT NULL,
+    kitchen_living INT NOT NULL,
+    tvroom_living INT NOT NULL,
+    patio_living INT NOT NULL,
+    pool_living INT NOT NULL,
+    barbecue_living INT NOT NULL,
+    image_living VARCHAR(60) NOT NULL,
+    id_typehouse INT NOT NULL,
+    CONSTRAINT fk_living_place_person FOREIGN KEY (id_person) REFERENCES persons(id_person),
+    CONSTRAINT fk_living_place_city FOREIGN KEY (id_city) REFERENCES cities(id_city),
+    CONSTRAINT fk_living_place_housetype FOREIGN KEY (id_typehouse) REFERENCES housetype(id_hausetype)
+);
 
 
 
